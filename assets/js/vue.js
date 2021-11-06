90f50650ab0539bebd16472e600c3c6541d5f956
Jessy BREYNE <jessybreyne@protonmail.com>
2021-11-06 10:57:32 +0100

    Improve the red fav like icon

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -14,6 +14,38 @@ const products = [
   { id: 12, description: 'Mesh Genova', price: 6, img: 'assets/img/mesh-genova.JPG'},
 ];
 
+// set little red dot above icons nav
+let red_icon = () => {
+  let checkbox = document.getElementsByTagName('input'); 
+  let notif = document.getElementById('nav-not');
+
+  // check if at least one of the checkboxes on the DOM is checked
+  const checkboxCheck = () => { 
+    for (let i = 0; i < checkbox.length; i++) {
+      if (checkbox[i].checked) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // set red dot
+  const setNotif = () => {
+    if (checkboxCheck()){
+      // console.log('true');
+      notif.style.visibility = 'visible';
+      notif.style.opacity = '1';
+    } else {
+      // console.log('false');
+      notif.style.visibility = 'hidden';
+      notif.style.opacity = '0';
+    }
+  }
+  
+  // check once if red dot is needed
+  setNotif();
+}
+
 const Home = {
     template: '#home',
     name: 'Home',
@@ -46,6 +78,9 @@ const Home = {
     },
     mounted: () => {
         this.getLikeCookie;
+    },
+    updated: () => {
+        red_icon();
     }
 }
 
